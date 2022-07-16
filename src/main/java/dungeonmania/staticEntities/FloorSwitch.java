e02189f9b0fd508c4f95bbd344397456e5901740
Dion Zhuo <z5258852@student.unsw.edu.au>
2022-07-16 14:45:12 +1000

    completed floor switch

--- src/main/java/dungeonmania/staticEntities/FloorSwitch.java
+++ src/main/java/dungeonmania/staticEntities/FloorSwitch.java
@@ -1,19 +1,39 @@
 package dungeonmania.staticEntities;
 
+import java.util.List;
+
+import dungeonmania.Entity;
+import dungeonmania.DungeonMap.DungeonMap;
 import dungeonmania.util.Position;
 
 public class FloorSwitch extends StaticEntity {
-    private boolean isTouch;
 
+    private boolean underBoulder;
+
+    /**
+     * Constructor for FloorSwitch
+     * @param id
+     * @param position
+     * @param type
+     */
     public FloorSwitch (String id, Position position, String type) {
         super(id, position, type);
     }
 
-    public boolean isTouch() {
-        return isTouch;
+    public boolean getUnderBoulder() {
+        return this.underBoulder;
     }
 
-    public void setTouch(boolean touch) {
-        isTouch = touch;
+    public void checkBoulder(DungeonMap dungeon) {
+        List<Entity> ent_in_position = dungeon.getMap().get(this.getPosition());
+        if (ent_in_position.isEmpty()) {
+            this.underBoulder = false; return;
+        }
+        for (Entity ent : ent_in_position) {
+            if (ent instanceof Boulder) {
+                this.underBoulder = true; return;
+            }
+        }
     }
+
 }
