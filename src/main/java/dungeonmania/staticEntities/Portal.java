2aea7c313015f9e99ca7642f298d3215769edc5c
Dion Zhuo <z5258852@student.unsw.edu.au>
2022-07-16 14:05:42 +1000

    added colour id to portal

--- src/main/java/dungeonmania/staticEntities/Portal.java
+++ src/main/java/dungeonmania/staticEntities/Portal.java
@@ -2,8 +2,25 @@ package dungeonmania.staticEntities;
 
 import dungeonmania.util.Position;
 
-public class Portal extends StaticEntity{
-    public Portal (String id, Position position, String type) {
+public class Portal extends StaticEntity {
+
+    private String colour_id;
+    private Position teleportal;
+
+    /**
+     * Constructor for Portal
+     * @param id
+     * @param position
+     * @param type
+     * @param colour_id
+     */
+    public Portal (String id, Position position, String type, String colour_id) {
         super(id, position, type);
+        this.colour_id = colour_id;
     }
+
+    public String getColour() {
+        return this.colour_id;
+    }
+
 }
