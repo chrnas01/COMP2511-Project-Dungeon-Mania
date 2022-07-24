43d814b1d9782a8b67e654972601f2f9dd9b13ff
shybeejd <shybeejd@gmail.com>
2022-07-24 13:27:41 +0800

    bfs for mercenary

--- src/main/java/dungeonmania/movingEntities/Mercenary.java
+++ src/main/java/dungeonmania/movingEntities/Mercenary.java
@@ -55,35 +55,36 @@ public class Mercenary extends MovingEntity {
     @Override
     public void move(Direction direction, DungeonMap dungeon) {
         this.bribe_amount = dungeon.getConfig().BRIBE_AMOUNT;
-        Position old = this.getPosition();
-        Position next_position = this.getPosition().translateBy(direction);
-        List<Entity> entities = dungeon.getMap().get(next_position);
-
-        if (entities == null) { 
-            dungeon.addPosition(next_position);
-            dungeon.moveEntity(this.getPosition(), next_position, this);
-            this.setPosition(next_position);
-            return;
-        }
-
-        for (Entity entity: entities) {
-            if (entity instanceof Wall || entity instanceof ZombieToastSpawner ) {return;}
-            if ((entity instanceof Boulder && ((Boulder) entity).moveDirection(dungeon, direction))
-                || (entity instanceof Door && ((Door) entity).getOpen())) {
-                dungeon.moveEntity(old, next_position, this);
-                this.setPosition(next_position);
-                return;
-            } else if (entity instanceof Boulder || entity instanceof Door) {
-                return;
-            } else if (entity instanceof Portal && ((Portal) entity).teleport(dungeon, direction, this)) {
-                dungeon.moveEntity(old, this.getPosition(), this);
-            } else if (entity instanceof Portal) {
-                return;
-            } else {
-                dungeon.moveEntity(old, next_position, this);
-                this.setPosition(next_position);
-            }
-        }
+//        Position old = this.getPosition();
+//        Position next_position = this.getPosition().translateBy(direction);
+//        List<Entity> entities = dungeon.getMap().get(next_position);
+//
+//        if (entities == null) {
+//            dungeon.addPosition(next_position);
+//            dungeon.moveEntity(this.getPosition(), next_position, this);
+//            this.setPosition(next_position);
+//            return;
+//        }
+//
+//        for (Entity entity: entities) {
+//            if (entity instanceof Wall || entity instanceof ZombieToastSpawner ) {return;}
+//            if ((entity instanceof Boulder && ((Boulder) entity).moveDirection(dungeon, direction))
+//                || (entity instanceof Door && ((Door) entity).getOpen())) {
+//                dungeon.moveEntity(old, next_position, this);
+//                this.setPosition(next_position);
+//                return;
+//            } else if (entity instanceof Boulder || entity instanceof Door) {
+//                return;
+//            } else if (entity instanceof Portal && ((Portal) entity).teleport(dungeon, direction, this)) {
+//                dungeon.moveEntity(old, this.getPosition(), this);
+//            } else if (entity instanceof Portal) {
+//                return;
+//            } else {
+//                dungeon.moveEntity(old, next_position, this);
+//                this.setPosition(next_position);
+//            }
+//        }
+        this.move(dungeon);
     }
 
     public Direction bfs(Position src,Position tar,DungeonMap dungeon){
